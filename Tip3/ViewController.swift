6e76a3c7e0d6a5eee53790722a62dd7dff85d7d5
Aman Kumar <32149831+amankumar98@users.noreply.github.com>
2019-12-21 21:27:57 -0800

    Initial Commit

---
+++ Tip3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip3
+//
+//  Created by Aman Kumar on 12/21/19.
+//  Copyright © 2019 Aman Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
